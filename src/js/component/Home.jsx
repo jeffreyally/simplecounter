383a40ae96362cfb4159c52871e81573e28a7e9c
jeffreyally <jeffreyally@gmail.com>
2022-02-01 01:39:21 +0000

    counter project so far

---
+++ src/js/component/Home.jsx
@@ -0,0 +1,22 @@
+import React from "react";
+
+//include images into your bundle
+import rigoImage from "../../img/rigo-baby.jpg";
+
+//create your first component
+export const Home = (props) => {
+	return (
+		<div class="vw-100 vh-100">
+			<div class="w-100 h-25 bg-dark text-white">
+				
+				<div class="text-white">{props.num1}</div>
+				<div class="text-white">{props.num2}</div>
+				<div class="text-white">{props.num3}</div>
+				<div class="text-white">{props.num4}</div>
+				<div class="text-white">{props.num5}</div>
+				<div class="text-white">{props.num6}</div>
+			</div>
+		</div>
+	);
+};
+//numbering props
--- src/js/index.js
+++ src/js/index.js
@@ -6,7 +6,25 @@ import ReactDOM from "react-dom";
 import "../styles/index.css";
 
 //import your own components
-import Home from "./component/home.jsx";
+import { Home } from "./component/Home.jsx";
 
 //render your react application
 ReactDOM.render(<Home />, document.querySelector("#app"));
+
+//set interval generates number
+//actively reload home component every sec
+//set interval calls function which executes previous two steps
+//pass numbers as props of home component
+
+let time = 0;
+
+const displaytimer = () => {
+	time = time + 1;
+    let timestring = '00000' + time
+	console.log(time);
+	ReactDOM.render(<Home num1='' num2=''  num3='' num4='' num5='' num6=''/>, document.querySelector("#app"))};
+//initialize variable outside and increment inside
+setInterval(displaytimer, 1000);
+
+//concatenate 5 zeros as a string for the timer
+//how to access string "index" extract seperate digits from string
